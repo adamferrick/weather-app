6c046a00453f313bcd251a208c63036eae130634
Adam Ferrick <adameferrick@gmail.com>
2023-08-11 01:46:41 -0700

    add color changing to the planets background

--- src/ui/background/background.js
+++ src/ui/background/background.js
@@ -76,14 +76,43 @@ export class Mountains extends Background {
 
 export class Planets extends Background {
   #sky;
+  #planet;
+  #moon1;
+  #moon2;
+  #hill;
+
   constructor(el, base) {
     super(el, base, planetsRaw);
     const skyEl = document.createElement("div");
     this.#sky = new Sky(skyEl, base);
     el.prepend(skyEl);
+    this.#planet = {
+      base: this.el.querySelector("#path35186"),
+      highlight: this.el.querySelector("#circle5740"),
+    };
+    this.#moon1 = {
+      base: this.el.querySelector("#circle29802"),
+      highlight: this.el.querySelector("#path304"),
+    };
+    this.#moon2 = {
+      base: this.el.querySelector("#path30241"),
+      highlight: this.el.querySelector("#circle31349"),
+    };
+    this.#hill = {
+      face: this.el.querySelector("#path7408"),
+      shadow: this.el.querySelector("#path7402"),
+    };
   }
 
   changeColor(color) {
     this.#sky.changeColor(color);
+    this.#planet.base.style.fill = color.mix(this.base, 0.7);
+    this.#planet.highlight.style.fill = color.mix(this.base, 0.5);
+    this.#moon1.base.style.fill = color.mix(this.base, 0.7);
+    this.#moon1.highlight.style.fill = color.mix(this.base, 0.5);
+    this.#moon2.base.style.fill = color.mix(this.base, 0.7);
+    this.#moon2.highlight.style.fill = color.mix(this.base, 0.5);
+    this.#hill.face.style.fill = color.mix(this.base, 0.6);
+    this.#hill.shadow.style.fill = color.mix(this.base, 0.7);
   }
 }
